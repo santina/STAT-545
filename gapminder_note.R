66bf564d93887d705a833fc0a76b924d0b90f644
Santina Lin <santina424@gmail.com>
2014-09-24 12:51:33 -0700

    small changes

---
+++ gapminder_note.R
@@ -0,0 +1,7 @@
+#Intro
+
+The following consists of some interesting observations obtained from 
+[Gapminder](http://www.gapminder.org/) with their data on [statistical records 
+on various countries] (http://www.stat.ubc.ca/~jenny/notOcto/STAT545A/examples/gapminder/data/gapminderDataFiveYear.txt)
+
+We will explore 
