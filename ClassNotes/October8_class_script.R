5ba5c20aa892df874ee3fd56bbfab64931b2543b
Santina Lin <santina424@gmail.com>
2014-10-08 11:54:05 -0700

    notes: reading/writing data and R objects, ordering factors

--- ClassNotes/October8_class_script.R
+++ ClassNotes/October8_class_script.R
@@ -25,8 +25,46 @@ write.table(data_sub, "data_sub.txt")
 
 #' try using some arguments in write.table()
 #' by default it's a space for delim 
+#' much prettier to see than txt on github 
 
 write.table(data_sub, "data_sub.csv", sep=",", row.names = FALSE, quote = FALSE)
 
 
+#inspect the levels of country : 
+order_data  <- factor(data_sub, levels = data_sub$country, ordered = TRUE)
 
+ordered  <- data_sub %>%
+  mutate(country=reorder(country, lifeExp, max))
+#compare the order of country specific to the lifeExp:  
+data.frame(levels(data_sub$country), levels(ordered$country))
+# the actual data frame doesn't change, but when you graph something you'll see it 
+
+#' write to file with saveRDS() 
+#' it would retain dataframe, factor ordering, can save any R objects
+#' not a file you can inspect, but a binary file 
+saveRDS(ordered, "ordered_lifeExp.rds")
+
+#' now remove it, and get it back from .rds file! 
+#
+rm(ordered) # delete from work space 
+ordered     # confirm it's deleted 
+ordered  <- readRDS("ordered_lifeExp.rds")
+str(ordered)
+levels(ordered$country) #same as before! 
+
+
+#' write to file with dput () 
+#' plain text representation of the object for R 
+dput(ordered, "ordered_dput.txt")
+
+# try delete and get it back 
+rm(ordered)
+ordered
+ordered  <- dget("ordered_dput.txt")
+str(ordered) # str(ordered, give.attr = FALSE)
+levels(ordered$country) #same as before! 
+
+
+
+# the only difference is really just having a viewable txt or binary 
+# also if many objects to store, just put them in a list 
