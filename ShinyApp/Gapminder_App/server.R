b7afea8be007a293b4b496b5848a5019e5e0ca8d
Santina Lin <santina424@gmail.com>
2014-11-26 12:11:15 -0800

    example script on shiny app

--- ShinyApp/Gapminder_App/server.R
+++ ShinyApp/Gapminder_App/server.R
@@ -1,19 +1,45 @@
 gpURL <- "http://tiny.cc/gapminder"
 data <- read.delim(file = gpURL) 
 library(dplyr)
+library(ggplot2)
 
 shinyServer(function(input, output){
-  
+  output$choose_country <- renderUI({ # get a country list
+  	selectInput("country_from_gapminder", "country", 
+  							as.list(levels(data$country)))
+  })
+	
+	one_country_data <- reactive({
+		
+		if(is.null(input$select_country)){
+			return(NULL)
+		}
+  	subset(data, country == input$select_country & 
+  				 	year %in% c(input$year_range[1]:input$year_range[2])) # get it from the UI! 
+  })
+	
+	
   # some functions do render data. 
   # which make interaction possible 
   output$gapminder_table <- renderTable({
-    years <- input$year_range
-    data %>% filter(country == input$select_country, year %in% c(years[1]:years[2]))  # get it from the UI! 
+  	one_country_data()
+
     
   })
   # another render function
   output$output_country <- renderText({
-    paste("Country selected", input$select_country) 
+  	paste("Country selected", input$select_country) 
+  })
+  output$info <- renderPrint({ #instead of renderText so that it prints out?
+  	str(input$year_range)
+  })
+  # this hold our plot
+  output$ggplot_gdppc_vs_country <- renderPlot({
+  	if(is.null(one_country_data)){
+  		return(NULL)
+  	}
+  	p <- ggplot(one_country_data(), aes(x = year, y= gdpPercap))
+  	p+ geom_point()
   })
 
   # if you want something to be interactive, 
--- ShinyApp/Gapminder_App/ui.R
+++ ShinyApp/Gapminder_App/ui.R
@@ -4,6 +4,7 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel("User input will be here", 
                  # make a drop down menu
+    						 #uiOuput("choose_country"),
                  selectInput("select_country", 
                              label = "Country", 
                              choices = list("Iraq", "Canada", "Mali")
@@ -15,11 +16,16 @@ shinyUI(fluidPage(
                              value = c(1955,2005), # set the starting value
                              format = "####"         
                 )
+    						 
             
-    ), 
+    ),
+    # the strings in those bracket are acutal name in server.R
     mainPanel("My cool graphs go here",
               textOutput("output_country"), # reference to server.R
-              tableOutput("gapminder_table") # must be the same in server.R
+              textOutput("info"), # add our select country to app
+    					plotOutput("ggplot_gdppc_vs_country"),
+    					tableOutput("gapminder_table") # must be the same in server.R
+    					
     )
   )
 ))
