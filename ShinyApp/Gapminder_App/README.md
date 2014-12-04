936b553d0f4727e22b2634ccd7ebf8bb7a4f11bc
Santina Lin <santina424@gmail.com>
2014-12-03 17:11:25 -0800

    spaces and tabs

--- ShinyApp/Gapminder_App/README.md
+++ ShinyApp/Gapminder_App/README.md
@@ -18,8 +18,8 @@ If I first wrap `input$dropdown` with another funtion, like `reactive({})` and a
 
 I really like the search box I made, which was taken from an example on the shiny gallery. It integrates with the server side so nicely. Besides that, I find shiny app harder to debut in general. Unlike other assignments/projects, you can type things into the console, try different code, before putting them in a R markdown or .R file. In Shiny, the variables are stored within the app and no accessible through the console. I think some of the things we are used to, such as `kable`, can't be used in shiny either (at least it throws me some error). At least, turning the background into something dark helps make the table look prettier.
 
-The `DESCRIPTION` file allows me to set show-case to default so that my peer can look at both README and code without having to make extra clicks
+The `DESCRIPTION` file allows me to set show-case to default so that my peer can look at both README and code while looking a shiny without having to make extra clicks on my github page. 
 
 # Credits 
 
-I credit my learning in this assignment to the [in class tutorial] (http://stat545-ubc.github.io/shiny03_activity.html) as well as various resources on the Shiny app website. I also use the css style sheet downloaded from [bootswatch's slate theme](http://bootswatch.com/slate/). The styling of the graph itself is done using the package `ggthemes`. 
+I credit my learning in this assignment to the [in class tutorial](http://stat545-ubc.github.io/shiny03_activity.html) as well as various resources on the Shiny app website. I also use the css style sheet downloaded from [bootswatch's slate theme](http://bootswatch.com/slate/). The styling of the graph itself is done using the package `ggthemes`. 
--- ShinyApp/Gapminder_App/ui.R
+++ ShinyApp/Gapminder_App/ui.R
@@ -3,26 +3,26 @@ shinyUI(fluidPage(theme = "bootstrap.css",
   
   sidebarLayout(
     sidebarPanel(
-                helpText(h5("Select your filtering criteria")),
-    						 uiOutput("choose_country"),
-                 sliderInput("year_range",
-                             label = h4("Range of years: "),
-                             min = 1952, # info we already know about our data
-                             max = 2007,
-                             value = c(1955,2005), # set the starting value
-                             format = "####"         
-                ), 
-    						 selectInput("dropdown", 
-    						             label = h4("Choose a variable"),
-    						             choices = c("Population", "GDP per Capita",
-    						                         "Life Expectancy"),
-    						             selected = "Population")						 
+      helpText(h5("Select your filtering criteria")),
+    	  uiOutput("choose_country"),
+          sliderInput("year_range",
+             label = h4("Range of years: "),
+             min = 1952, # info we already know about our data
+             max = 2007,
+             value = c(1955,2005), # set the starting value
+             format = "####"         
+          ), 
+  				selectInput("dropdown", 
+             label = h4("Choose a variable"),
+             choices = c("Population", "GDP per Capita",
+                         "Life Expectancy"),
+             selected = "Population")						 
             
-    ),
+         ),
     # the strings in those bracket are acutal name in server.R
     mainPanel(
-    					plotOutput("ggplot_gdppc_vs_country"),
-    					tableOutput("gapminder_table") # must be the same in server.R
+			plotOutput("ggplot_gdppc_vs_country"),
+			tableOutput("gapminder_table") # must be the same in server.R
     					
     )
   )
